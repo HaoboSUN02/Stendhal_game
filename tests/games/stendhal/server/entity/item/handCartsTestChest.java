2a54133aded351360f2b43b9ed3d5dc9a82e7c85
Yuwei Sang <yuwei.sang@student.manchester.ac.uk>
2022-12-05 11:31:01 +0000

    test for hand carts use/open

--- tests/games/stendhal/server/entity/item/handCartsTestChest.java
+++ tests/games/stendhal/server/entity/item/handCartsTestChest.java
@@ -1,5 +1,47 @@
 package games.stendhal.server.entity.item;
 
-public class handCartsTestChest {
+import games.stendhal.server.maps.MockStendlRPWorld;
+import marauroa.common.Log4J;
+import utilities.PlayerTestHelper;
+import utilities.RPClass.BlockTestHelper;
+import utilities.RPClass.ItemTestHelper;
 
+public class handCartsTestChest {
+	
+	    //@BeforeClass
+		public static void setUpBeforeClass() {
+			
+			BlockTestHelper.generateRPClasses();
+			PlayerTestHelper.generatePlayerRPClasses();
+			MockStendlRPWorld.get();
+			ItemTestHelper.generateRPClasses();
+			Log4J.init();
+		}
+		
+		//test if the hand cart open or close
+		//@Test
+		public void testForOpen() {
+			
+			HandCart hc = new HandCart();
+			
+			//at the begin, the hand cart is closed
+			assertFalse(hc.isOpen());
+			
+			//open the hand cart
+			hc.open();
+			aseertTrue(hc.isOpen());
+			
+			//after that close the hand cart
+			hc.close();
+			assertFalse(hc.isOpen());
+			
+		}
+		
+		//test when hand carts move, the hand carts should close
+		//public void testForMoveClose() {
+			
+			
+		//}
+		
+		
 }
